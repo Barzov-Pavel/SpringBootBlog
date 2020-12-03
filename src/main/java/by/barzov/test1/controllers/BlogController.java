39a5b4cde526615827ca0c3e02b2f56262c36522
pavel <barzov_pavel@mail.ru>
2020-12-03 10:02:43 +0300

    Add watching article, counting views, page about

--- src/main/java/by/barzov/test1/controllers/BlogController.java
+++ src/main/java/by/barzov/test1/controllers/BlogController.java
@@ -6,8 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.Optional;
 
 @Controller
 public class BlogController
@@ -36,5 +40,26 @@ public class BlogController
         Post post = new Post(title, anons, full_text);
         postRepository.save(post);
         return "redirect:/blog";
+    }
+
+    @GetMapping("/blog/{id}")
+    public String blogDetails(@PathVariable(value = "id") long id, Model model)
+    {
+        if (!postRepository.existsById(id))
+        {
+            return "redirect:/blog";
+        }
+        Optional<Post> optionalPost = postRepository.findById(id);
+
+        // add view
+        Post post = optionalPost.get();
+        post.setViews(post.getViews() + 1);
+        postRepository.save(post);
+
+        ArrayList<Post> result = new ArrayList<>();
+        result.add(post);
+
+        model.addAttribute("post", result);
+        return "blog-details";
     }
 }
