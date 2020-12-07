117f37d810858550842c7a14f24ca96e1fd8927b
pavel <barzov_pavel@mail.ru>
2020-12-07 17:48:58 +0300

    Little changes

--- src/main/java/by/barzov/test1/aspect/ProfilingHandlerBeanPostProcessor.java
+++ src/main/java/by/barzov/test1/aspect/ProfilingHandlerBeanPostProcessor.java
@@ -4,8 +4,6 @@ import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import net.sf.cglib.proxy.InvocationHandler;
 
-import java.lang.reflect.Method;
-
 import net.sf.cglib.proxy.Enhancer;
 import org.springframework.stereotype.Component;
 
@@ -15,7 +13,7 @@ import java.util.Map;
 @Component
 public class ProfilingHandlerBeanPostProcessor implements BeanPostProcessor
 {
-    private Map<String, Class> map = new HashMap<>();
+    private final Map<String, Class<?>> map = new HashMap<>();
 
     @Override
     public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException
@@ -31,20 +29,15 @@ public class ProfilingHandlerBeanPostProcessor implements BeanPostProcessor
     @Override
     public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException
     {
-        Class beanClass = map.get(beanName);
+        Class<?> beanClass = map.get(beanName);
         if (beanClass != null)
         {
-            return Enhancer.create(beanClass, new InvocationHandler()
-            {
-                @Override
-                public Object invoke(Object proxy, Method method, Object[] args) throws Throwable
-                {
-                    long start = System.nanoTime();
-                    Object retVal = method.invoke(bean, args);
-                    long end = System.nanoTime();
-                    System.out.println("runtime of method: " + method.getName() + " " + (end - start));
-                    return retVal;
-                }
+            return Enhancer.create(beanClass, (InvocationHandler) (proxy, method, args) -> {
+                long start = System.nanoTime();
+                Object retVal = method.invoke(bean, args);
+                long end = System.nanoTime();
+                System.out.println("runtime of method: " + method.getName() + " " + (end - start));
+                return retVal;
             });
         }
         return bean;
